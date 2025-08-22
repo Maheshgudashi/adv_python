5513d33d7672b39d622bfaf3c5ce15587a97b7f9
Maheshgudashi <>
2025-08-22 15:11:43 +0530

    main.py done

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+def simple_intersest(p: float, n: float, r: float) -> tuple[float,float]:
+    """
+    This fuction calculates simple interest and amount
+    """
+    i = (p * n * r) / 100
+    a = p + i
+    return i, a 
+
 def main():
-    print("Hello from adv-python-1130!")
+   p = float(input("Principal in INR : "))
+   n = float(input("Number of Years :"))
+   r = float(input("Rate of interest in %p.a."))
+   i, a = simple_intersest(p, n, r)
+   print(f"Simple Interest : {i:.2f} INR")
+   print(f"Amount : {a:.2f} INR")
 
 
 if __name__ == "__main__":
     main()
+    
